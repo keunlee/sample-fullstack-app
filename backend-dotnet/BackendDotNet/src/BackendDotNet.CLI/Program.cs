08bc6c3fec436e052d0cc7b19194b130cbeeee97
kelee <keun.lee@contractor.cengage.com>
2016-03-21 17:01:42 -0500

    added CLI library

--- backend-dotnet/BackendDotNet/src/BackendDotNet.CLI/Program.cs
+++ backend-dotnet/BackendDotNet/src/BackendDotNet.CLI/Program.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BackendDotNet.Support;
+using CommandLine;
 
 namespace BackendDotNet {
 	public class Program {
 		public static void Main(string[] args) {
-			System.Console.WriteLine ("Hello World");
+			var result = CommandLine.Parser.Default.ParseArguments<CLIOptions> (args);
+
+			result.MapResult (o => {
+				var fullPath = Path.GetFullPath( o.InputFile );
+				if ( File.Exists( fullPath )) {
+					
+				}
+				return 0;
+			}, errs => 1);
 		}
 	}
 }
---
+++ backend-dotnet/BackendDotNet/src/BackendDotNet.CLI/Support/CLIOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using CommandLine;
+using CommandLine.Text;
+
+namespace BackendDotNet.Support {
+	public class CLIOptions {
+		[Option('f', "file", Required = true, HelpText = "Input file to read.")]
+		public string InputFile { get; set; }
+	}
+}
