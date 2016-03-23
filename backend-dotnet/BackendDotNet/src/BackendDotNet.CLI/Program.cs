7c6d09bf8273cf56d79898065617b8d79d18351d
kelee <keun.lee@contractor.cengage.com>
2016-03-23 01:59:41 -0500

    clean up

--- backend-dotnet/BackendDotNet/src/BackendDotNet.CLI/Program.cs
+++ backend-dotnet/BackendDotNet/src/BackendDotNet.CLI/Program.cs
@@ -2,34 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Autofac;
+using BackendDotNet.Common.DepedencyInjection;
 using BackendDotNet.Service;
 using BackendDotNet.Service.Impl;
 using BackendDotNet.Support;
 using CommandLine;
-using Autofac;
 
 namespace BackendDotNet {
 	public class Program {
 
 		public static void Main(string[] args) {
-			NHibernateHelper helper = new NHibernateHelper ();
-			helper.Initialize ();
-			helper.doSampleRun ();
-			/*
-			var builder = new ContainerBuilder ();
-			builder.RegisterType<StockServiceImpl> ().As<IStockService> ();
-			Container = builder.Build();
-
-			var result = CommandLine.Parser.Default.ParseArguments<CLIOptions> (args);
+			var bootStrapper = new AutofacBootStrapper ();
+			var stockService = bootStrapper.getContainer ().Resolve<IStockService> ();
+			var result = Parser.Default.ParseArguments<CLIOptions> (args);
 
 			result.MapResult (o => {
 				var fullPath = Path.GetFullPath( o.InputFile );
 				if ( File.Exists( fullPath )) {
-					doFileImport( fullPath );
+					stockService.importStocksByCSVFile( fullPath );
 				}
 				return 0;
 			}, errs => 1);
-			*/
 		}
 	}
 }
