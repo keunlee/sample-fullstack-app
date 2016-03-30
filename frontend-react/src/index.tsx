d4fe05bb853827ec1b390988be2c2a79f426ea6f
kelee <keun.lee@contractor.cengage.com>
2016-03-29 22:32:24 -0500

    initial commit react frontend

---
+++ frontend-react/src/index.tsx
@@ -0,0 +1,8 @@
+/// <reference path='../typings/main.d.ts'/>
+
+import React = require('react');
+import ReactDOM = require('react-dom');
+
+
+// Render a simple React h1 component into the body.
+ReactDOM.render(<h1>Sup G!!!!</h1>, document.getElementsByTagName('body')[0]);
