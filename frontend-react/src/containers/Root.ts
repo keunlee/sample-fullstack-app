c333118360050caa815441515cdcaa7b3a003f55
kelee <keun.lee@contractor.cengage.com>
2016-03-31 16:11:20 -0500

    add root to app

---
+++ frontend-react/src/containers/Root.ts
@@ -0,0 +1,6 @@
+/// <reference path='../../typings/main.d.ts'/>
+
+import React = require('react');
+
+export default class Root extends React.Component<{}, void> {
+}
--- frontend-react/src/index.tsx
+++ frontend-react/src/index.tsx
@@ -2,7 +2,12 @@
 
 import React = require('react');
 import ReactDOM = require('react-dom');
+import Root from './containers/Root';
 let css = require("./styles/index.scss");
 
 // Render a simple React h1 component into the body.
-ReactDOM.render(<h1>Hello Typescript!</h1>, document.getElementsByTagName('body')[0]);
+// ReactDOM.render(<h1>Hello Typescript!</h1>, document.getElementsByTagName('body')[0]);
+
+const renderNode = document.getElementById('root');
+
+ReactDOM.render(<Root/>, renderNode);
